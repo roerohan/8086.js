9416f8b80bc0a7ca26d01f0fc200b5cf025f7b3b
Rohan Mukherjee <rohanbayern@gmail.com>
2020-09-02 04:05:22 +0530

    feat: Partition page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,44 @@
 import React from 'react';
-import AceEditor from 'react-ace';
 
+import AceEditor from 'react-ace';
 import 'ace-builds/src-noconflict/mode-assembly_x86';
 import 'ace-builds/src-noconflict/theme-dracula';
 
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        height: '100vh',
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
+
 export default function Home() {
+    const classes = useStyles();
+
     return (
-        <AceEditor
-            mode="assembly_x86"
-            theme="dracula"
-            // onChange={onChange}
-            name="editor"
-            editorProps={{ $blockScrolling: true }}
-        />
+        <div className={classes.root}>
+            <Grid container spacing={3}>
+                <Grid item xs={6}>
+                    <Paper className={classes.paper}>xs=6</Paper>
+                </Grid>
+                <Grid item xs={6}>
+                    <AceEditor
+                        mode="assembly_x86"
+                        theme="dracula"
+                        // onChange={onChange}
+                        name="editor"
+                        editorProps={{ $blockScrolling: true }}
+                    />
+                </Grid>
+            </Grid>
+        </div>
     );
 }
