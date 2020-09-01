a609d3f82e32b84c56f544f3c9a56efed6fb896c
Rohan Mukherjee <rohanbayern@gmail.com>
2020-09-02 03:38:01 +0530

    feat: Editor added

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import AceEditor from 'react-ace';
+
+import 'ace-builds/src-noconflict/mode-assembly_x86';
+import 'ace-builds/src-noconflict/theme-dracula';
 
 export default function Home() {
     return (
-        <div>Hello World!</div>
+        <AceEditor
+            mode="assembly_x86"
+            theme="dracula"
+            // onChange={onChange}
+            name="editor"
+            editorProps={{ $blockScrolling: true }}
+        />
     );
 }
