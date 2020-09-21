ddd5e572135ca7c33b973116c6e83d40eadb3965
Rohan Mukherjee <rohanbayern@gmail.com>
2020-09-22 04:43:17 +0530

    feat: Added tests

---
+++ src/emulator/test/lexer.js
@@ -0,0 +1,13 @@
+import Lexer from '../parser/lexer.js';
+
+function tokenize(code) {
+    console.log(`\nCode: ${code}`);
+    const lex = new Lexer(code);
+    return lex.tokenize();
+}
+
+console.log(tokenize('aaa'));
+console.log(tokenize('mul al'));
+console.log(tokenize('add ax, bx'));
+console.log(tokenize('mov dx, \'hello\''));
+console.log(tokenize('call someFunction'));
