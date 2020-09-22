0153a7b2b2f1684c4579497a54f047ca9485f527
Rohan Mukherjee <rohanbayern@gmail.com>
2020-09-22 17:33:54 +0530

    feat: Added string token

--- src/emulator/models/index.js
+++ src/emulator/models/index.js
@@ -1,7 +1,8 @@
+export { default as StringToken } from './stringToken.js';
+export { default as NewLine } from './newLine.js';
+export { default as Comment } from './comment.js';
 export { default as Register } from './register.js';
 export { default as Mnemonic } from './mnemonic.js';
-export { default as Comment } from './comment.js';
-export { default as NewLine } from './newLine.js';
 export { default as Separator } from './separator.js';
 export {
     InvalidTokenError,
---
+++ src/emulator/models/stringToken.js
@@ -0,0 +1,6 @@
+export default class StringToken {
+    constructor({ value, position }) {
+        this.value = value;
+        this.position = position;
+    }
+}
