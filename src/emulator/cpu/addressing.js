afeb744acaf55106c1b52aeca9694ccece18af9f
Rohan Mukherjee <rohanbayern@gmail.com>
2020-09-23 04:43:53 +0530

    refactor: Cleanup addressing

--- src/emulator/cpu/addressing.js
+++ src/emulator/cpu/addressing.js
@@ -8,42 +8,55 @@ export default class Addressing {
         if (!op) {
             return null;
         }
+
+        const { regs } = this.registers;
+
         switch (op.type) {
         case 'IMMEDIATE':
             return op.value;
+
         case 'REGISTER':
             if (['L', 'H'].includes(op.value[1])) {
                 if (!['A', 'B', 'C', 'D'].includes(op.value[0])) {
                     throw Error("Only AX,BX,CX,DX registers can have 'L' or 'H' suffix");
                 }
-                return this.registers.regs[`${op.value[0]}X`].get(op.value[1]);
+                return regs[`${op.value[0]}X`].get(op.value[1]);
             }
-            return this.registers.regs[op.value].get();
+            return regs[op.value].get();
+
         case 'MEMORY':
-            return this.memory.get(this.registers.regs.DS.get() + op.value);
+            return this.memory.get(regs.DS.get() + op.value);
+
         case 'RELATIVE':
             throw Error('Not implemented');
+
         default:
             throw Error("Invalid Adressing mode, this shoudln't happen ideally");
         }
     }
 
     set(op, value) {
+        const { regs } = this.registers;
+
         switch (op.type) {
         case 'IMMEDIATE':
             throw Error("Can't set to immediate value???");
+
         case 'REGISTER':
             if (['L', 'H'].includes(op.value[1])) {
                 if (!['A', 'B', 'C', 'D'].includes(op.value[0])) {
                     throw Error("Only AX,BX,CX,DX registers can have 'L' or 'H' suffix");
                 }
-                return this.registers.regs[`${op.value[0]}X`].set(value, op.value[1]);
+                return regs[`${op.value[0]}X`].set(value, op.value[1]);
             }
-            return this.registers.regs[op.value].set(value);
+            return regs[op.value].set(value);
+
         case 'MEMORY':
-            return this.memory.set(this.registers.regs.DS.get() + op.value, value);
+            return this.memory.set(regs.DS.get() + op.value, value);
+
         case 'RELATIVE':
             throw Error('Not implemented');
+
         default:
             throw Error("Invalid Adressing mode, this shoudln't happen ideally");
         }
