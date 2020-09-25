14bd0a48db85aa22920079cc9ff6f8af6e27c54f
Jose (Darkcoder) <j0s3s3gur4@gmail.com>
2020-09-25 11:55:57 -0400

    feat: add annotations when raise a Syntax error

--- src/components/Editor/index.jsx
+++ src/components/Editor/index.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import AceEditor from 'react-ace';
 import 'ace-builds/src-noconflict/mode-assembly_x86';
 import 'ace-builds/src-noconflict/theme-dracula';
 
-import { updateCode } from 'slices/emulatorSlice';
+import { updateCode, selectError, selectCode } from 'slices/emulatorSlice';
 import ButtonsContainer from 'components/ButtonsContainer';
 import NotificationError from 'components/NotificationError';
 
 export default function Editor() {
     const dispatch = useDispatch();
+
+    const error = useSelector(selectError);
+    const codeStorage = useSelector(selectCode);
+    let annotation = {};
+
+    if (error.isRaised) {
+        annotation = {
+            row: error.lineNumber - 1,
+            column: 0,
+            type: 'error',
+            text: `${error.name}: ${error.message || error.token}`,
+        };
+    }
 
     const onChange = (code) => {
         dispatch(updateCode(code));
@@ -34,12 +47,14 @@ export default function Editor() {
                 mode="assembly_x86"
                 fontSize="1rem"
                 theme="dracula"
+                cursorStart={6}
                 onChange={onChange}
-                value={defaultMsg}
+                value={codeStorage || defaultMsg}
                 showPrintMargin={false}
                 height="100vh"
                 width="50vw"
                 name="editor"
+                annotations={[annotation]}
                 editorProps={{ $blockScrolling: true }}
             />
         </div>
