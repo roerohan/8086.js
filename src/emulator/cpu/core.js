b73af57cbc74d73835259e71cbd85384999309de
Joao Victor <vikitor566@gmail.com>
2020-09-30 23:03:02 -0300

    Add instruction INC

--- src/emulator/cpu/core.js
+++ src/emulator/cpu/core.js
@@ -89,6 +89,10 @@ export default class CPU {
             }
             break;
 
+        case 'INC':
+            setAddr(op1, getAddr(op1) + 1);
+            break;
+
         case 'DIV':
             if (op1.size === 8) {
                 const al = regs.AX.get('l') / getAddr(op1);
