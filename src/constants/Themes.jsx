f325949746089ef98f3591d42f22891ccf5557e0
Namit Nathwani <namsnath@gmail.com>
2020-10-01 17:42:50 +0530

    feat: adds themes to match editor themes

--- src/constants/Themes.jsx
+++ src/constants/Themes.jsx
@@ -3,7 +3,6 @@ import { createMuiTheme } from '@material-ui/core';
 const Themes = {
     dracula: createMuiTheme({
         palette: {
-            backgroudColor: '',
             background: {
                 main: '#282A36',
                 raised: '#21222C',
@@ -17,66 +16,96 @@ const Themes = {
             },
         },
     }),
-    vs2015: {
-        fgColor: '#C3DDE1',
-        bgColor: '#1E1E1E',
-        dark: '#44AAE6',
-        primary: '#E89440',
-        primText: '#FFFFFF',
-        error: '#FF578F',
-        default: '#44475a',
-        warning: '',
-    },
-    'atom-one-dark': {
-        fgColor: '#ABB2BF',
-        bgColor: '#282C34',
-        dark: '#E06C75',
-        primary: '#5FA5D6',
-        primText: '#FFFFFF',
-        error: '#E06C75',
-        default: '#44475a',
-        warning: '',
-    },
-    monokai: {
-        fgColor: '#F9F8F5',
-        bgColor: '#272822',
-        dark: '#75715E',
-        primary: '#2AA198',
-        primText: '#FFFFFF',
-        error: '#F92672',
-        default: '#44475a',
-        warning: '',
-    },
-    'gruvbox-dark': {
-        fgColor: '#EBDBB2',
-        bgColor: '#282828',
-        dark: '#75715E',
-        primary: '#83A598',
-        primText: '#FFFFFF',
-        error: '#CC241D',
-        default: '#44475a',
-        warning: '',
-    },
-    'solarized-light': {
-        fgColor: '#073642',
-        bgColor: '#FDF6E3',
-        dark: '#073642',
-        primary: '#586E75',
-        primText: '#FFFFFF',
-        error: '#CB4B16',
-        default: '#44475a',
-        warning: '',
-    },
-    'tomorrow-night': {
-        fgColor: '#F0C574',
-        bgColor: '#1D1F21',
-        dark: '#343A40',
-        primary: '#8ABDB6',
-        primText: '#FFFEFE',
-        error: '#CC6666',
-        default: '#44475a',
-        warning: '',
-    },
+    terminal: createMuiTheme({
+        palette: {
+            background: {
+                main: '#000000',
+                raised: '#1a0005',
+            },
+            text: {
+                primary: '#ff6347',
+                secondary: '#4682b4',
+            },
+            border: {
+                main: '#4682b4',
+            },
+        },
+    }),
+    xcode: createMuiTheme({
+        palette: {
+            background: {
+                main: '#FFFFFF',
+                raised: '#e8e8e8',
+            },
+            text: {
+                primary: '#333',
+                secondary: '#008E00',
+            },
+            border: {
+                main: '#008E00',
+            },
+        },
+    }),
+    monokai: createMuiTheme({
+        palette: {
+            background: {
+                main: '#272822',
+                raised: '#2F3129',
+            },
+            text: {
+                primary: '#66D9EF',
+                secondary: '#75715E',
+            },
+            border: {
+                main: '#75715E',
+            },
+        },
+    }),
+    chrome: createMuiTheme({
+        palette: {
+            background: {
+                main: '#FFFFFF',
+                raised: '#EBEBEB',
+            },
+            text: {
+                primary: '#000000',
+                secondary: '#236E24',
+            },
+            border: {
+                main: '#236E24',
+            },
+        },
+    }),
+    github: createMuiTheme({
+        palette: {
+            background: {
+                main: '#FFFFFF',
+                raised: '#E8E8E8',
+            },
+            text: {
+                primary: '#000000',
+                secondary: '#998',
+            },
+            border: {
+                main: '#998',
+            },
+        },
+    }),
+    gruvbox: createMuiTheme({
+        palette: {
+            background: {
+                main: '#1D2021',
+                raised: '#1D2021',
+            },
+            text: {
+                primary: '#EBDAB4',
+                secondary: '#928375',
+            },
+            border: {
+                main: '#928375',
+            },
+        },
+    }),
 };
 
 export default Themes;
